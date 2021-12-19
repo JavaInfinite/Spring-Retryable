53b3b42750fe5e81f1450618d7d30f3ee7dcf15b
JavaInfinite <javainfinite1987@gmail.com>
2021-12-19 09:00:46 +0530

    Custom Exception Included

--- Retry/src/main/java/com/javainfinite/retry/controller/RestRetryController.java
+++ Retry/src/main/java/com/javainfinite/retry/controller/RestRetryController.java
@@ -1,5 +1,6 @@
 package com.javainfinite.retry.controller;
 
+import com.javainfinite.retry.exception.RetryException;
 import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Recover;
 import org.springframework.retry.annotation.Retryable;
@@ -9,32 +10,30 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/RestRetry")
 public class RestRetryController {
-	
-	@RequestMapping(value="/retry")
-	@Retryable(value = { Exception.class }, maxAttempts = 4, label="retry API", backoff = @Backoff(delay = 5000))
-	public void RetryTest() throws Exception {
-		System.out.println("Retrying Attempt...");
-		int i=0;
-		if(i==0) {
-			throw new ArithmeticException("Failed retry Again....");
-		}
-	}
-	
-	@RequestMapping(value="/retryExclude")
-	@Retryable(value = { ArithmeticException.class }, maxAttempts = 4, exclude=ArithmeticException.class, label="retry API", backoff = @Backoff(delay = 5000))
-	public void RetryTestExclude() throws Exception {
-		System.out.println("Retrying Attempt...");
-		try {
-			int i = 10/0;
-		} catch(ArithmeticException e) {
-			System.out.println("Arithmetic Exception...Will not retry");
-		} catch (Exception e) {
-			System.out.println("Exception Exception...");
-		}
-	}
-	
-	@Recover
-    public void connectionException(ArithmeticException e) {
+
+    @RequestMapping(value = "/retry")
+    @Retryable(value = {RetryException.class}, maxAttempts = 4, label = "retry API", backoff = @Backoff(delay = 5000))
+    public void RetryTest() throws RetryException {
+        System.out.println("Retrying Attempt...");
+        int i = 0;
+        if (i == 0) {
+            throw new RetryException("Failed retry Again....");
+        }
+    }
+
+    @RequestMapping(value = "/retryExclude")
+    @Retryable(value = {RetryException.class}, maxAttempts = 4, exclude = RetryException.class, label = "retry API", backoff = @Backoff(delay = 5000))
+    public void RetryTestExclude() throws Exception {
+        System.out.println("Retrying Attempt...");
+        int i = 0;
+        if (i == 0) {
+            throw new RetryException("Failed retry Again....");
+        }
+
+    }
+
+    @Recover
+    public void connectionException(RetryException e) {
         System.out.println("Retry failure");
     }
 
